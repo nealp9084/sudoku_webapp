24836f25f14fa19cf7fb7976d4f5c6f91bd304a2
Neal Patel <nealp9084@gmail.com>
2013-05-06 02:05:50 -0400

    Initial commit, created a new web app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SudokuWebapp::Application.config.secret_token = 'c0c589e06d265a95a8f6a31d48ddfc99adb25f0d87d7e8b947551f9f34c13683e3b20e26502445f7e4820e6a7ce4fd64ee09c308c62b0f4dc71e867e65da9eeb'
